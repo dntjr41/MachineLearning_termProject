d4c7ba6a72091a068ef4cc9b37138b33acd44626
wooseok <qkqh8639@gmail.com>
2021-11-02 23:29:49 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Import Class Libraries
+import pandas as pd
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+###################################################################
+# Read File
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Cran.all.1400.txt:
+# Number of documents – 1,400 documents
+# Fields - (i) I: document id
+#         (ii) T: short description of documents
+#        (iii) A: author
+#         (iv) B: book
+#          (v) W: content of documents
+
+# Cran.qry.txt:
+# Number of queries – 225 queries
+# Field - (i) I: query id
+#        (ii) W: content of query
+
+# Cranqrel.txt:
+# relevant documents for each query (target file). It contains series of query order,
+# relevant document id and relevance between query and document.
+# Field – (i) Query order: order of appearance of the query in cran.qry
+#        (ii) Relevant document id
+#       (iii) Relevance between query and document range 1 to 5
+#             (As the number goes from 1 to 5, it means that the relevance decrease)
+
+document = 'cran.all.1400.txt'
+query = 'cran.qry.txt'
+qrel = 'cranqrel.txt'
+
