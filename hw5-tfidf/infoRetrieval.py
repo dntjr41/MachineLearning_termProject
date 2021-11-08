dec6a145d70ef456270a67b3481409b22a8d784a
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-11-09 00:52:24 +0900

    Update infoRetrieval.py

--- hw5-tfidf/infoRetrieval.py
+++ hw5-tfidf/infoRetrieval.py
@@ -19,6 +19,7 @@ def parseQuery(filename):
     cont = False
     string = ""
     for line in f:
+      #print(line)
       if ".I" in line:
         cont = False
         if len(string) > 0:
@@ -39,9 +40,6 @@ def parseQuery(filename):
     for count in range(length):
       I = IDS[count]
       qu = new_queries[count]
-      q=Query(I,qu)
-      # print("q.id :",q.ID)
-      #print("q.query: ",q.query)
       query_docs.append(Query(I,qu))
     f.close()
     return query_docs
@@ -70,6 +68,8 @@ def parseAbsDocs(filename):
     f.close()
     return new_abstracts
 
+
+
 def tokenize(docs, docType="query"):
   if docType == "query":
     qToks = []
@@ -82,19 +82,16 @@ def tokenize(docs, docType="query"):
       sentences = nltk.sent_tokenize(abstract)
       toks = []
       for sentence in sentences:
-        stopset = [word for word in stopwords.words('english')]
+        stopset = [word for word in stop_list.closed_class_stop_words]
         stop_punc = list(string.punctuation)
         stops = stopset+stop_punc
         tokens = nltk.wordpunct_tokenize(sentence)
+        lemmatizer = WordNetLemmatizer()
         tokens = [w for w in tokens if w.lower() not in stops ]
+        tokens = [w.lower() for w in tokens]
+        [lemmatizer.lemmatize(w) for w in tokens]
         filtered_tokens = [x for x in tokens if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
-        # lemmatization
-        wnl = WordNetLemmatizer()
-        lamm_tokens = [wnl.lemmatize(token) for token in filtered_tokens]
-        # stemming
-        pst=PorterStemmer()
-        stemmed_tokens=[pst.stem(token) for token in lamm_tokens]
-        toks.append(stemmed_tokens)
+        toks.append(filtered_tokens)
       absToks.append(toks)
     return absToks
 
@@ -119,88 +116,5 @@ def organize(tokens, docType):
           else:
             dic[tok][count] = dic[tok][count] + 1
     count = count + 1
-
   return dic
 
-def tfidf(sentence, tfDic, idfDic):
-  vector = []
-  for token in sentence:
-    tf = tfDic.get(token, 0)
-    idf = idfDic.get(token, 0)
-    vector.append(float(tf*idf))
-  return vector
-
-def idf(qDic,totalDocs):
-  dic = {}
-  for key in qDic:
-    numDocs = 0
-    for count in qDic[key]:
-      if count > 0:
-        numDocs = numDocs + 1
-    dic[key] = math.log(float(totalDocs) / float(numDocs))
-  return dic
-
-def abstractTf(absDic, totalDocs):
-  absTf = []
-  for count in range(totalDocs):
-    dic = {}
-    for key in absDic:
-      term = absDic[key][count]
-      if term > 0:
-        dic[key] = term
-    absTf.append(dic)
-  return absTf
-
-def queryTf(qToks):
-  tf = []
-  for query in qToks:
-    dic = {}
-    for token in query:
-      if token not in dic:
-        dic[token] = 1
-      else:
-        dic[token] = dic[token] + 1
-    for key in dic:
-      dic[key] = float(dic[key])
-    tf.append(dic)
-  return tf
-
-
-def cosSim(vect1, vect2):
-  numerator = 0
-  # sos == sum of squares
-  sos1, sos2 = 0, 0
-  for index in range(len(vect1)):
-    numerator = numerator + vect1[index]*vect2[index]
-    sos1 = sos1 + math.pow(vect1[index],2)
-    sos2 = sos2 + math.pow(vect2[index],2)
-  sos1 = math.sqrt(sos1)
-  sos2 = math.sqrt(sos2)
-  denominator = float(sos1 * sos2)
-  divide = 0
-  try:
-    divide = float(numerator/denominator)
-  except:
-    divide = 0
-  return divide
-
-def score(queries, absTf, absIdf, qTf, qIdf):
-  scores = []
-  countQ = 0
-  for query in queries:
-    v1 = tfidf(query, qTf[countQ], qIdf)
-    countA = 0
-    score_tups = []
-    for abstract in absTf:
-      v2 = tfidf(query, absTf[countA], absIdf)
-      cosine_sim = cosSim(v1,v2)
-      out = (countQ + 1, countA + 1, cosine_sim)
-      score_tups.append(out)
-      countA += 1
-    countQ += 1
-    scores.append(score_tups)
-  sortedScores = []
-  for query in scores:
-    sortedScore = sorted(query, key=lambda tup: tup[2], reverse = True )
-    sortedScores.append(sortedScore)
-  return sortedScores
